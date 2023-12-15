ecde349e0239fc6908d40324479b1a6f5f87fa11
ilyas <zhuanyshev.ilyas@gmail.com>
2023-12-15 20:05:24 +0600

    MESSAGE

---
+++ src/main/java/kz/bitlab/rabbit/middle02rabbitreceiver/Middle02rabbitreceiverApplication.java
@@ -0,0 +1,13 @@
+package kz.bitlab.rabbit.middle02rabbitreceiver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Middle02rabbitreceiverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Middle02rabbitreceiverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/bitlab/rabbit/middle02rabbitreceiver/Middle02rabbitreceiverApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.bitlab.rabbit.middle02rabbitreceiver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Middle02rabbitreceiverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
